140ff9d7558a24dc59c54d1af6f36b4ffab15363
joaoalberis <alberisjoao@hotmail.com>
2023-07-10 17:41:36 -0300

    FInalização do hamburuger para mobile, finalização da visualização para desktop

--- ChallengesWebResponsive/interiorconsultant/index.js
+++ ChallengesWebResponsive/interiorconsultant/index.js
@@ -1,10 +1,7 @@
 const hamburguer = document.querySelector('.hamburguer')
 const menuBar = document.querySelector('.bar')
-const main = document.querySelector('main')
-const h1 = document.querySelector('header h1')
 
 hamburguer.addEventListener('click', () => {
     menuBar.classList.toggle('open')
-    main.classList.toggle('open')
-    h1.classList.toggle('open')
+    hamburguer.classList.toggle('open')
 })
